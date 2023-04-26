f80c2aa76532699ee6b03fa4be990b1ddcb906f5
marthacaps <108442221+marthacaps@users.noreply.github.com>
2023-04-26 16:51:15 +0800

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Tortang Talong and Dried Fish')
-streamlit.text('Oatmeal with fresh fruit salad')
-streamlit.text('Fried Rice with Tapa and Egg')
+streamlit.text('🥑🍞Tortang Talong and Avocado toast')
+streamlit.text('🥣🥗🥑Oatmeal with fresh fruit salad')
+streamlit.text('🐔Fried Rice with tocino and Egg')
+
+streamlit.header(''🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
