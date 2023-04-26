e3a1883ba0a5d447e1f6f6de5a83d0dbbcadff59
marthacaps <108442221+marthacaps@users.noreply.github.com>
2023-04-26 16:48:27 +0800

    Update streamlit.app.py
    added header, and text

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,4 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.body('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Tortang Talong and Dried Fish')
+streamlit.text('Oatmeal with fresh fruit salad')
+streamlit.text('Fried Rice with Tapa and Egg')
